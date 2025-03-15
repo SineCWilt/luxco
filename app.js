c82a2aad9e6e1d05571c42cf88c0f9704bbf3a29
SineCWilt <sine.wilt22@gmail.com>
2025-03-15 18:47:57 -0500

    Completed version of webpage

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const mobileMenu = document.getElementById("mobile-menu");
 const navMenu = document.querySelector(".nav-menu");
+const mainButton = document.querySelector(".main-btn"); // Select the button
 
 mobileMenu.addEventListener("click", () => {
     navMenu.classList.toggle("active");
     mobileMenu.classList.toggle("is-active");
+
+    // Hide the button when menu is expanded, show it when collapsed
+    if (window.innerWidth <= 768) {
+        mainButton.classList.toggle("hide-btn");
+    }
 });
+
